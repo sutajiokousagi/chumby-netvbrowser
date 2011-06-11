cc5e0b8858fd211756ec8bb2bed5b18247dc954c
Sean Cross <sean@chumby.com>
2011-06-11 22:17:43 +0800

    chumby-netvbrowser: Replace scripts with init.d script

--- chumby-netvbrowser.sh
+++ chumby-netvbrowser.sh
@@ -4,9 +4,7 @@
 case "$1" in
 	start)
 		# Start in the background so we don't hog the console
-		# This app uses QtGui hence requires -qws option,
-		# but does not render anything to the framebuffer
-		NeTVBrowser -qws 2>&1 2> /dev/null&
+		NeTVBrowser -qws SetUrl http://localhost 2>&1 > /dev/null &
 		;;
 
 	stop)

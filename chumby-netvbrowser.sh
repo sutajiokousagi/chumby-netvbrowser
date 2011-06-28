98f3dc29952689fee0b38b49e8c07c9ce7c31d03
Torin <torin@chumby.com>
2011-06-28 16:28:58 +0800

    Added temp fix for "Semop lock/unlock failure removed" error message flood.

--- chumby-netvbrowser.sh
+++ chumby-netvbrowser.sh
@@ -5,6 +5,11 @@ case "$1" in
 	start)
 		# Start in the background so we don't hog the console
 		NeTVBrowser -qws -nomouse SetUrl http://localhost 2>&1 > /dev/null &
+
+		# Temp fix for "Semop lock/unlock failure Identifier removed" flood
+		sleep 2
+		rm -rf /tmp/qtembedded-0
+
 		;;
 
 	stop)

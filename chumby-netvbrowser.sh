0763dadd168069991843d74b00d4c891da4e443f
Torin <torin@chumby.com>
2011-07-15 11:46:39 +0800

    Start-up script needs to use absolute path to index.html as NeTVServer has not started yet (http://localhost returns nothing)

--- chumby-netvbrowser.sh
+++ chumby-netvbrowser.sh
@@ -5,22 +5,25 @@ case "$1" in
 	start)
 		export QWS_KEYBOARD=chumbyirkb
 
-		# Start in the background so we don't hog the console
-		NeTVBrowser -qws -nomouse SetUrl http://localhost 2>&1 > /dev/null &
-
 		# Temp fix for "Semop lock/unlock failure Identifier removed" flood
-		sleep 2
 		rm -rf /tmp/qtembedded-0
 
+		# Start in the background so we don't hog the console
+		NeTVBrowser -qws -nomouse SetUrl /usr/share/netvserver/docroot/index.html 2>&1 > /dev/null &
 		;;
 
 	stop)
 		killall NeTVBrowser 2> /dev/null
 		;;
 
+	restart)
+		$0 stop
+		$0 start
+		;;
+
 	*)
 
-		echo "Usage: $0 {start|stop}"
+		echo "Usage: $0 {start|stop|restart}"
 		;;
 esac
 

1e7d14015efdfdf4acfc92d7add66e5a92597f1f
Torin <torin@chumby.com>
2011-07-15 16:37:04 +0800

    temp Semop was in the wrong line

--- chumby-netvbrowser.sh
+++ chumby-netvbrowser.sh
@@ -5,9 +5,6 @@ case "$1" in
 	start)
 		export QWS_KEYBOARD=chumbyirkb
 
-		# Temp fix for "Semop lock/unlock failure Identifier removed" flood
-		rm -rf /tmp/qtembedded-0
-
 		# Start in the background so we don't hog the console
 		# if NeTVServer is not up, we have to use absolute path, else http://localhost returns nothing
 		# when server is up, we should use http://localhost for POST/GET to work
@@ -17,6 +14,10 @@ case "$1" in
 		else
 			NeTVBrowser -qws -nomouse SetUrl /usr/share/netvserver/docroot/index.html 2>&1 > /dev/null &
 		fi
+
+		# Temp fix for "Semop lock/unlock failure Identifier removed" flood
+		# Should be after we started the Qt process
+		rm -rf /tmp/qtembedded-0
 		;;
 
 	stop)

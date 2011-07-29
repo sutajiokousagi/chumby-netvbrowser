0fef12d7856bf91d18b3194cbbef0f46af38aa41
Torin <torin@chumby.com>
2011-07-29 13:16:39 +0800

    browser now runs as the one & only QWS server

--- chumby-netvbrowser.sh
+++ chumby-netvbrowser.sh
@@ -3,15 +3,30 @@
 
 case "$1" in
 	start)
+		# hide the ugly initialization sequence
+		setbox 0 0 1 1
+
+		# prefix chroma key
+		fpga_ctl w 0xc 2
+		fpga_ctl w 0xd 240
+		fpga_ctl w 0xe 0
+		fpga_ctl w 0xf 240
+
+		# use irkb driver as a normal keyboard		
+		export QWS_KEYBOARD=chumbyirkb
+
 		# [Temp] Hide the flash player
 		setplayer c 0 0 1 1
 		setplayer p
 
-		# [Temp] Mute the browser complains
+		# [Temp] Mute the browser alignment complains
 		echo 2 > /proc/cpu/alignment
 
 		# Start in the background so we don't hog the console
-		NeTVBrowser SetUrl http://localhost 2>&1 > /dev/null &
+		NeTVBrowser -qws SetUrl http://localhost 2>&1 > /dev/null &
+
+		# fullscreen
+		setbox 0 0 1279 719
 		;;
 
 	stop)

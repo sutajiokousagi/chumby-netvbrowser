e601dc53487b4e33306d7627c91e509dd9932873
Torin <torin@chumby.com>
2011-07-29 15:50:14 +0800

    minor clean up on start up script

--- chumby-netvbrowser.sh
+++ chumby-netvbrowser.sh
@@ -7,20 +7,20 @@ case "$1" in
 		setbox 0 0 1 1
 
 		# prefix chroma key
-		fpga_ctl w 0xc 2
 		fpga_ctl w 0xd 240
 		fpga_ctl w 0xe 0
 		fpga_ctl w 0xf 240
+		fpga_ctl w 0xc 2
 
-		# use irkb driver as a normal keyboard		
-		export QWS_KEYBOARD=chumbyirkb
+		# [Temp] Mute the browser alignment complains
+		echo 2 > /proc/cpu/alignment
 
 		# [Temp] Hide the flash player
 		setplayer c 0 0 1 1
 		setplayer p
 
-		# [Temp] Mute the browser alignment complains
-		echo 2 > /proc/cpu/alignment
+		# use irkb driver as a normal keyboard		
+		export QWS_KEYBOARD=chumbyirkb
 
 		# Start in the background so we don't hog the console
 		NeTVBrowser -qws SetUrl http://localhost 2>&1 > /dev/null &

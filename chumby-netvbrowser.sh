e969dba3975647393a4c559aa0e84e1ccb408157
Torin <torin@chumby.com>
2011-08-02 14:35:54 +0800

    avoid printing out stuff that affects 'update/upgrade testing'

--- chumby-netvbrowser.sh
+++ chumby-netvbrowser.sh
@@ -4,20 +4,20 @@
 case "$1" in
 	start)
 		# hide the ugly initialization sequence
-		setbox 0 0 1 1
+		$(setbox 0 0 1 1)
 
 		# prefix chroma key
-		fpga_ctl w 0xd 240
-		fpga_ctl w 0xe 0
-		fpga_ctl w 0xf 240
-		fpga_ctl w 0xc 2
+		$(fpga_ctl w 0xd 240)
+		$(fpga_ctl w 0xe 0)
+		$(fpga_ctl w 0xf 240)
+		$(fpga_ctl w 0xc 2)
 
 		# [Temp] Mute the browser alignment complains
 		echo 2 > /proc/cpu/alignment
 
 		# [Temp] Hide the flash player
-		setplayer c 0 0 1 1
-		setplayer p
+		$(setplayer c 0 0 1 1)
+		$(setplayer p)
 
 		# use irkb driver as a normal keyboard		
 		export QWS_KEYBOARD=chumbyirkb
@@ -26,7 +26,7 @@ case "$1" in
 		NeTVBrowser -qws SetUrl http://localhost > /dev/null 2>&1 &
 
 		# fullscreen
-		setbox 0 0 1279 719
+		$(setbox 0 0 1279 719)
 		;;
 
 	stop)

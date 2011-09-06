3b8dbd9b1f33a93f87a9936ee045c39672bb89d8
Torin <torin@chumby.com>
2011-09-06 16:28:48 +0800

    small bug: timer for keystroke should be 2 seconds

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -21,7 +21,7 @@ MainWindow::MainWindow(QWidget *parent) :
     this->port = DEFAULT_PORT;
 
     up = 0;down = 0;left = 0;right = 0;center = 0;cpanel = 0;widget = 0;hidden1 = 0; hidden2 = 0;
-    keyStrokeTimer.setInterval(1000);
+    keyStrokeTimer.setInterval(2000);
     keyStrokeTimer.setSingleShot(true);
     keyStrokeTimer.stop();
     QObject::connect(&keyStrokeTimer, SIGNAL(timeout()), this, SLOT(slot_keyStrokeTimeout()));

b50360f4f09f7a050212111b337e5f374bc2ec8f
Torin <torin@chumby.com>
2011-09-07 16:19:46 +0800

    syntax error

--- mainwindow_common.cpp
+++ mainwindow_common.cpp
@@ -43,8 +43,8 @@ void MainWindow::slot_keepAliveTimeout()
     //We don't need to check if fCheckAlive exist or not. It will failed anyway.
     QString isAlive = this->myWebView->page()->mainFrame()->evaluateJavaScript( QString("fCheckAlive();") ).toString();
 
-    if (isAlive == "true")      qDebug("%s: [keep alive] [OK] %s", TAG, this->myWebView->page()->mainFrame()->url());
-    else                        qDebug("%s: [keep alive] [FAILED] %s", TAG, this->myWebView->page()->mainFrame()->url());
+    if (isAlive == "true")      qDebug("%s: [keep alive] [OK] %s", TAG, this->myWebView->page()->mainFrame()->url().toString().toLatin1().constData());
+    else                        qDebug("%s: [keep alive] [FAILED] %s", TAG, this->myWebView->page()->mainFrame()->url().toString().toLatin1().constData());
 
     if (isAlive == "true")
         return;
